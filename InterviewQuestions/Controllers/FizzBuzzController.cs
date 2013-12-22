bcb8252439e4dc3b521463393cf8da3b6f62d485
satrent <trent@copperconsultingmn.com>
2013-12-21 23:22:39 -0600

    intial check-in

---
+++ InterviewQuestions/Controllers/FizzBuzzController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InterviewQuestions.Models;
+
+namespace InterviewQuestions.Controllers
+{
+    public class FizzBuzzController : Controller
+    {
+        //
+        // GET: /FizzBuzz/
+
+        public ActionResult Question1()
+        {
+            var model = new FizzBuzzVM();
+
+            model.Numbers.Add(1);
+
+
+            return View(model);
+        }
+
+    }
+}
---
+++ InterviewQuestions/Models/FizzBuzzVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InterviewQuestions.Models
+{
+    public class FizzBuzzVM
+    {
+        public FizzBuzzVM()
+        {
+            Numbers = new List<int>();
+        }
+
+        public List<int> Numbers { get; set; }
+    }
+}
